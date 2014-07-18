3c393ddb9223935aa9f2a7859d84ee7abc5c53f5
johannesprinz <johannesprinz@gmail.com>
2014-07-18 12:32:47 +1200

    Stubbed out solution template

---
+++ Application/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace JohannesPrinz.Sandpit.Application
+{
+    public class Constants
+    {
+        public class Assembly
+        {
+            public const string Company = "Johannes.Prinz";
+            public const string Copyright = "Copyright ©  2014";
+            public const string Trademark = "Johannes Prinz";
+            public const string Culture = "en-US";
+            public const bool ComVisible = false;
+            //Major, Minor, Build, Revision
+            public const string AssemblyVersion = "1.0.0.0";
+            public const string AssemblyFileVersion = "1.0.0.0";
+        }
+    }
+}
--- UI.Console/Properties/AssemblyInfo.cs
+++ UI.Console/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using JohannesPrinz.Sandpit.Application;
 
-[assembly: AssemblyTitle("Sandpit Unit Tests")]
+[assembly: AssemblyTitle("Sandpit Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("JohannesPrinz.Sandpit.Tests.Unit")]
-[assembly: Guid("4fd1d3cb-c433-4e5f-9dc5-989f1e4f5cc4")]
+[assembly: AssemblyProduct("JohannesPrinz.Sandpit.UI.Console")]
+[assembly: Guid("81f3d8bf-a85a-45f4-bc6c-4a14070d29d5")]
+[assembly: AssemblyCulture("")]
 [assembly: AssemblyCompany(Constants.Assembly.Company)]
 [assembly: AssemblyCopyright(Constants.Assembly.Copyright)]
 [assembly: AssemblyTrademark(Constants.Assembly.Trademark)]
-[assembly: AssemblyCulture(Constants.Assembly.Culture)]
 [assembly: ComVisible(Constants.Assembly.ComVisible)]
 [assembly: AssemblyVersion(Constants.Assembly.AssemblyVersion)]
 [assembly: AssemblyFileVersion(Constants.Assembly.AssemblyFileVersion)]
